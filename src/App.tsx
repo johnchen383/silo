6843212c0b5d966a04dfc5d4195063a9af360e83
johnchen383 <john.bm.chen@gmail.com>
2025-09-26 00:05:35 +1200

    calculate height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { CONST_DEFAULT_CHAPTER_URL } from "./consts/bible_data";
 import Scaffold from "./components/Scaffold";
 import { useAuth } from "./providers/auth_provider";
 import Login from "./components/Login";
-import { type JSX } from "react";
+import { useEffect, type JSX } from "react";
 
 function PrivateRoute({ children }: { children: JSX.Element }) {
   const { user } = useAuth();
@@ -19,12 +19,23 @@ const App = () => {
     return <></>;
   }
 
+  useEffect(() => {
+    function setFullHeight() {
+      document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+    }
+
+    window.addEventListener('resize', setFullHeight);
+    setFullHeight();
+
+    return () => window.removeEventListener('resize', setFullHeight);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<ScaffoldWrapper />}>
           <Route path="/" element={<Navigate to={`${CONST_DEFAULT_CHAPTER_URL}`} replace />} />
-          <Route path="/login" element={ <Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/bible/:book/:chapter/:verse?" element={<PrivateRoute><Chapter /></PrivateRoute>} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
