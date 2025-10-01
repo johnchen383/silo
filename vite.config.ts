588cfe1a4c5b93d725e95d400b87d29d7c3bc611
johnchen383 <john.bm.chen@gmail.com>
2025-10-01 22:28:44 +1300

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -24,17 +24,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/temp.png',
+            src: '/silo_icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/temp.png',
+            src: '/silo_icon.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/temp.png',
+            src: '/silo_icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
