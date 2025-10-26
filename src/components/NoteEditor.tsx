791791d331246a614bf95c9e4534202acb814068
johnchen383 <john.bm.chen@gmail.com>
2025-10-26 13:02:44 +1100

    fix

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -6,10 +6,6 @@ import { CONST_BOOK_SYMBOL_TO_NAME } from "../consts/bible_data";
 const NoteEditor = () => {
     const {pendingNote, setPendingNote} = useNoteProvider();
 
-    if (!pendingNote) {
-        return <></>;
-    }
-
     return (
         <div id="DOC_EL_NOTE_EDITOR" className="note-editor">
             <div className="exit">
@@ -19,9 +15,9 @@ const NoteEditor = () => {
                     document.getElementById("DOC_EL_NOTE_EDITOR_FILLER")?.classList.remove("active");
                 }} />
             </div>
-            <div className="heading">{CONST_BOOK_SYMBOL_TO_NAME[pendingNote.start.book!]} {pendingNote.start.chapter}:{pendingNote.start.verse}</div>
+            <div className="heading">{CONST_BOOK_SYMBOL_TO_NAME[pendingNote?.start.book!]} {pendingNote?.start.chapter}:{pendingNote?.start.verse}</div>
             <div className="content">
-                {pendingNote.content}
+                {pendingNote?.content}
             </div>
         </div>
     )
